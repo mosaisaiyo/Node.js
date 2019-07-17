14c71015669eb3d37ac1130a82a4d91f05a601f8
Ryan Xiong <43770753+mosaisaiyo@users.noreply.github.com>
2019-07-17 11:45:15 +0800

    update js files

--- myBatchTool.js
+++ myBatchTool.js
@@ -7,19 +7,19 @@ function myBatchTool() {
   this.batchList = [];
   this.batchNum = 0;
 
-  this.initialize = function() {
+  this.initialize = function () {
     this.batchList = [];
     this.batchNum = 0;
   }
 
-  this.batchFinalize = function() {
+  this.batchFinalize = function () {
 
   }
 
-  this._batchComplete = function(me,id) {
-    for(var i=0;i<me.batchList.length;i++) {
-      if (me.batchList[i].id===id) {
-	me.batchList[i].complete = true;
+  this._batchComplete = function (me, id) {
+    for (var i = 0; i < me.batchList.length; i++) {
+      if (me.batchList[i].id === id) {
+        me.batchList[i].complete = true;
       }
 
       if (!me.batchList[i].complete) return false;
@@ -28,29 +28,29 @@ function myBatchTool() {
     me.batchFinalize();
   }
 
-  this.regBatch = function() {
+  this.regBatch = function () {
     var f = {};
     this.batchList.push(f);
-    f.id = 'batch-'+this.batchList.length;
+    f.id = 'batch-' + this.batchList.length;
     f.me = this;
     f.complete = false;
-    f.execBody = function() {};
-    f.callback = function() {
+    f.execBody = function () { };
+    f.callback = function () {
       f.execBody.apply(f.me, arguments);
       f.me._batchComplete(f.me, f.id);
     };
     return f;
   }
-  
-  this.addBatchTask = function(taskBody, taskComplete) {
-    if (!typeof taskBody==='function') throw TypeError('taskBody is not function!');
-        
+
+  this.addBatchTask = function (taskBody, taskComplete) {
+    if (!typeof taskBody === 'function') throw TypeError('taskBody is not function!');
+
     var f = {};
     this.batchList.push(f);
-    f.id = 'batch-'+this.batchList.length;
+    f.id = 'batch-' + this.batchList.length;
     f.me = this;
     f.complete = false;
-    f.callback = function() {
+    f.callback = function () {
       f.me._batchComplete(f.me, f.id);
     };
     taskComplete = f.callback;
--- myMongoTool.js
+++ myMongoTool.js
@@ -8,27 +8,27 @@ function myMongoTool() {
   this.Url = "mongodb://localhost:27017/";
   this.MongoClient = require('mongodb').MongoClient;
   this.DBConnection = undefined;
-  this.eventEmitter = new events.EventEmitter();	
+  this.eventEmitter = new events.EventEmitter();
 
-  this.connectDB = function(callback) {
+  this.connectDB = function (callback) {
     if (!this.DBConnection) {
       this._regConnectCallback(this, callback);
-      this._connectDB(this, this.MongoClient, this.Url, this._setConnection);      
+      this._connectDB(this, this.MongoClient, this.Url, this._setConnection);
     } else {
       callback.apply(this, arguments);
     }
 
   };
-  
-  this.isConnected = function() {
-    return this.DBConnection?true:false;
+
+  this.isConnected = function () {
+    return this.DBConnection ? true : false;
   }
 
-  this.disconnectDB = function() {
+  this.disconnectDB = function () {
     if (this.DBConnection) this.DBConnection.close();
   }
 
-  this.insertOne = function(insertOption, callback) {
+  this.insertOne = function (insertOption, callback) {
     if (!insertOption) throw Error('insertOption is undefined!');
     if (!callback) throw Error('callback is undefined!');
     if (!typeof callback === 'function') throw Error('callback is not function!');
@@ -43,7 +43,7 @@ function myMongoTool() {
     this._insertOne(this, dbName, collection, data, callback);
   }
 
-  this.insertMany = function(insertOption, callback) {
+  this.insertMany = function (insertOption, callback) {
     if (!insertOption) throw Error('insertOption is undefined!');
     if (!callback) throw Error('callback is undefined!');
     if (!typeof callback === 'function') throw Error('callback is not function!');
@@ -58,7 +58,7 @@ function myMongoTool() {
     this._insertMany(this, dbName, collection, data, callback);
   }
 
-  this.findRec = function(findOption, callback) {
+  this.findRec = function (findOption, callback) {
     if (!findOption) throw Error('findOption is undefined!');
     if (!callback) throw Error('callback is undefined!');
     if (!typeof callback === 'function') throw Error('callback is not function!');
@@ -73,7 +73,7 @@ function myMongoTool() {
     this._findRec(this, dbName, collection, whereStr, callback);
   }
 
-  this.updateRec = function(updateOption, callback) {
+  this.updateRec = function (updateOption, callback) {
     if (!updateOption) throw Error('updateOption is undefined!');
     if (!callback) throw Error('callback is undefined!');
     if (!typeof callback === 'function') throw Error('callback is not function!');
@@ -89,29 +89,29 @@ function myMongoTool() {
 
     this._updateRec(this, dbName, collection, whereStr, updateStr, callback);
   }
-  
-  this.getInsertOption = function(dbName, collection, data) {
+
+  this.getInsertOption = function (dbName, collection, data) {
     return Object.create({
-      dbName : dbName,
-      collection : collection,
-      data : data
+      dbName: dbName,
+      collection: collection,
+      data: data
     });
   }
-  
-  this.getFindOption = function(dbName, collection, whereStr) {
+
+  this.getFindOption = function (dbName, collection, whereStr) {
     return Object.create({
-      dbName : dbName,
-      collection : collection,
-      whereStr : whereStr
+      dbName: dbName,
+      collection: collection,
+      whereStr: whereStr
     });
   }
-  
-  this.getUpdateOption = function(dbName, collection, whereStr, updateStr) {
+
+  this.getUpdateOption = function (dbName, collection, whereStr, updateStr) {
     return Object.create({
-      dbName : dbName,
-      collection : collection,
-      whereStr : whereStr,
-      updateStr : updateStr
+      dbName: dbName,
+      collection: collection,
+      whereStr: whereStr,
+      updateStr: updateStr
     });
   }
 
@@ -120,153 +120,153 @@ function myMongoTool() {
 }
 
 myMongoTool.prototype = {
-    _regConnectCallback : function(me, callback) {
-      me.eventEmitter.on('dbConnected', callback);
-    },
+  _regConnectCallback: function (me, callback) {
+    me.eventEmitter.on('dbConnected', callback);
+  },
 
-    _setConnection : function(me, conn) {
-      me.DBConnection = conn;
-      me.eventEmitter.emit('dbConnected');
-    },
+  _setConnection: function (me, conn) {
+    me.DBConnection = conn;
+    me.eventEmitter.emit('dbConnected');
+  },
 
-    _connectDB : function(me, client, url, callback) {
-      if (client) {
-	var option = { useNewUrlParser: true };
-	client.connect(url, option, function(err, dbConn) {
-	  if (err) throw err;
-	  console.log('mongoDB connected!');
-	  callback(me, dbConn);
-	});
-      }
-    },
-
-    _insertOne : function(me, dbName, collection, data, callback) {
-
-      if (me.DBConnection) {
-
-	var dbo = me.DBConnection.db(dbName);
-
-	dbo.collection(collection).insertOne(data, function(err, res) {
-	  if (err) throw err;
-	  console.log("Document insert successfully!");
-	  callback(res);
-	});
-
-      } else {
-	(function(client, url, DBConnection, dbName, collection, data, callback) {
-	  if (client) {
-	    client.connect(url, function(err, DBConnection) {
-	      if (err) throw err;
-
-	      var dbo = DBConnection.db(dbName);
-
-	      dbo.collection(collection).insertOne(data, function(err, res) {
-		if (err) throw err;
-		console.log("Document insert successfully!");
-		callback(res);
-	      });
-	    });
-
-	  } else 
-	    throw Error('Cannot get module mongodb!');
-	})(me.MongoClient, me.Url, me.DBConnection, dbName, collection, data, callback);
-      }
-
-    },
-
-    _insertMany : function(me, dbName, collection, data, callback) {
-
-      if (me.DBConnection) {
-
-	var dbo = me.DBConnection.db(dbName);
-
-	dbo.collection(collection).insertMany(data, function(err, res) {
-	  if (err) throw err;
-	  console.log("Document insert successfully!");
-	  callback(res);
-	});
-
-      } else {
-	(function(client, url, DBConnection, dbName, collection, data, callback) {
-	  if (client) {
-	    client.connect(url, function(err, DBConnection) {
-	      if (err) throw err;
-
-	      var dbo = DBConnection.db(dbName);
-
-	      dbo.collection(collection).insertMany(data, function(err, res) {
-		if (err) throw err;
-		console.log("Document insert successfully!");
-		callback(res);
-	      });
-	    });
-
-	  } else 
-	    throw Error('Cannot get module mongodb!');
-	})(me.MongoClient, me.Url, me.DBConnection, dbName, collection, data, callback);
-      }
-
-    },
-
-    _findRec : function(me, dbName, collection, whereStr, callback) {
-      if (me.DBConnection) {
-	var dbo = me.DBConnection.db(dbName);
-	dbo.collection(collection).find(whereStr).toArray(function(err, result) {
-	  if (err) throw err; 
-	  console.log('Search complete!');
-	  callback(result);
-	});
-      } else {
-	(function(client, url, DBConnection, dbName, collection, whereStr, callback) {
-	  if (client) {
-	    client.connect(url, function(err, DBConnection) {
-	      if (err) throw err;
-
-	      var dbo = DBConnection.db(dbName);
-
-	      dbo.collection(collection).find(whereStr).toArray(function(err, result) {
-		if (err) throw err; 
-		console.log('Search complete!');
-		callback(result);
-	      });
-	    });
-
-	  } else 
-	    throw Error('Cannot get module mongodb!');
-	})(me.MongoClient, me.Url, me.DBConnection, dbName, collection, whereStr, callback);
-      }
-    },
-
-    _updateRec : function(me, dbName, collection, whereStr, updateStr, callback) {
-      if (me.DBConnection) {
-	var dbo = me.DBConnection.db(dbName);
-
-	dbo.collection(collection).updateMany(whereStr, updateStr, function(err, res) {
-	  if (err) throw err;
-	  console.log('Update complete!');
-	  callback(res);
-	});
-
-      } else {
-	(function(client, url, DBConnection, dbName, collection, whereStr, updateStr, callback) {
-	  if (client) {
-	    client.connect(url, function(err, DBConnection) {
-	      if (err) throw err;
-
-	      var dbo = DBConnection.db(dbName);
-
-	      dbo.collection(collection).updateMany(whereStr, updateStr, function(err, res) {
-		if (err) throw err;
-		console.log('Update complete!');
-		callback(res);
-	      });
-	    });
-
-	  } else 
-	    throw Error('Cannot get module mongodb!');
-	})(me.MongoClient, me.Url, me.DBConnection, dbName, collection, whereStr, updateStr, callback);
-      }
+  _connectDB: function (me, client, url, callback) {
+    if (client) {
+      var option = { useNewUrlParser: true };
+      client.connect(url, option, function (err, dbConn) {
+        if (err) throw err;
+        console.log('mongoDB connected!');
+        callback(me, dbConn);
+      });
     }
+  },
+
+  _insertOne: function (me, dbName, collection, data, callback) {
+
+    if (me.DBConnection) {
+
+      var dbo = me.DBConnection.db(dbName);
+
+      dbo.collection(collection).insertOne(data, function (err, res) {
+        if (err) throw err;
+        console.log("Document insert successfully!");
+        callback(res);
+      });
+
+    } else {
+      (function (client, url, DBConnection, dbName, collection, data, callback) {
+        if (client) {
+          client.connect(url, function (err, DBConnection) {
+            if (err) throw err;
+
+            var dbo = DBConnection.db(dbName);
+
+            dbo.collection(collection).insertOne(data, function (err, res) {
+              if (err) throw err;
+              console.log("Document insert successfully!");
+              callback(res);
+            });
+          });
+
+        } else
+          throw Error('Cannot get module mongodb!');
+      })(me.MongoClient, me.Url, me.DBConnection, dbName, collection, data, callback);
+    }
+
+  },
+
+  _insertMany: function (me, dbName, collection, data, callback) {
+
+    if (me.DBConnection) {
+
+      var dbo = me.DBConnection.db(dbName);
+
+      dbo.collection(collection).insertMany(data, function (err, res) {
+        if (err) throw err;
+        console.log("Document insert successfully!");
+        callback(res);
+      });
+
+    } else {
+      (function (client, url, DBConnection, dbName, collection, data, callback) {
+        if (client) {
+          client.connect(url, function (err, DBConnection) {
+            if (err) throw err;
+
+            var dbo = DBConnection.db(dbName);
+
+            dbo.collection(collection).insertMany(data, function (err, res) {
+              if (err) throw err;
+              console.log("Document insert successfully!");
+              callback(res);
+            });
+          });
+
+        } else
+          throw Error('Cannot get module mongodb!');
+      })(me.MongoClient, me.Url, me.DBConnection, dbName, collection, data, callback);
+    }
+
+  },
+
+  _findRec: function (me, dbName, collection, whereStr, callback) {
+    if (me.DBConnection) {
+      var dbo = me.DBConnection.db(dbName);
+      dbo.collection(collection).find(whereStr).toArray(function (err, result) {
+        if (err) throw err;
+        console.log('Search complete!');
+        callback(result);
+      });
+    } else {
+      (function (client, url, DBConnection, dbName, collection, whereStr, callback) {
+        if (client) {
+          client.connect(url, function (err, DBConnection) {
+            if (err) throw err;
+
+            var dbo = DBConnection.db(dbName);
+
+            dbo.collection(collection).find(whereStr).toArray(function (err, result) {
+              if (err) throw err;
+              console.log('Search complete!');
+              callback(result);
+            });
+          });
+
+        } else
+          throw Error('Cannot get module mongodb!');
+      })(me.MongoClient, me.Url, me.DBConnection, dbName, collection, whereStr, callback);
+    }
+  },
+
+  _updateRec: function (me, dbName, collection, whereStr, updateStr, callback) {
+    if (me.DBConnection) {
+      var dbo = me.DBConnection.db(dbName);
+
+      dbo.collection(collection).updateMany(whereStr, updateStr, function (err, res) {
+        if (err) throw err;
+        console.log('Update complete!');
+        callback(res);
+      });
+
+    } else {
+      (function (client, url, DBConnection, dbName, collection, whereStr, updateStr, callback) {
+        if (client) {
+          client.connect(url, function (err, DBConnection) {
+            if (err) throw err;
+
+            var dbo = DBConnection.db(dbName);
+
+            dbo.collection(collection).updateMany(whereStr, updateStr, function (err, res) {
+              if (err) throw err;
+              console.log('Update complete!');
+              callback(res);
+            });
+          });
+
+        } else
+          throw Error('Cannot get module mongodb!');
+      })(me.MongoClient, me.Url, me.DBConnection, dbName, collection, whereStr, updateStr, callback);
+    }
+  }
 }
 
 module.exports = myMongoTool;
